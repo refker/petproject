fadc467f4214b06ba31e606a21b392b9cf65d49f
andrey.minakov <andrey.minakov@roonyx.team>
2020-08-12 22:57:47 +0300

    add iframe

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,15 @@
 class MainController < ApplicationController
   def root
+
+  end
+
+  def update
+    redirect_to action: 'iframe', url: params[:url], company_id:  params[:company_id]
+  end
+
+  def iframe
+    @company_id = params[:company_id]
+    @uri = URI.parse(params[:url])
+    @js = "http://#{@uri.hostname}/reseller.js"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'main#root'
-
+  post '/', to: 'main#update'
+  get 'iframe', to: 'main#iframe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
